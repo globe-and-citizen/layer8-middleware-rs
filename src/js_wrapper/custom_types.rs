6c9dba8c2d7e69290641bfbe05d2bfe06ed3621b
Osoro Bironga <fanosoro@gmail.com>
2024-12-18 21:54:24 +0300

    allow for overrides

--- src/js_wrapper/custom_types.rs
+++ src/js_wrapper/custom_types.rs
@@ -4,6 +4,13 @@ use wasm_bindgen::prelude::*;
 /// This is necessary to preserve type information for the generated JS API.
 #[wasm_bindgen(getter_with_clone)]
 pub struct AssetsFunctionsWrapper {
+    /// This function is used to upload a file to the server.
+    ///   * @param {string} file_name: The name of the property in the request body object that contains the file name.
+    ///  * @returns {(req: any, res: any, next?: () => void) => void}: The next argument is optional and will be called if provided.
     pub single: js_sys::Function,
+
+    /// This function is used to upload a files to the server.
+    ///  * @param {string} file_collection_name: The name of the property in the request body object that contains the array of file names.
+    /// * @returns {(req: any, res: any, next?: () => void) => void}: The next argument is optional and will be called if provided.
     pub array: js_sys::Function,
 }
