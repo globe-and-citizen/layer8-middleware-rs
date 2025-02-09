31e37a99631b5394fc27a65f31f6c713b51969c7
Osoro Bironga <fanosoro@gmail.com>
2025-02-09 07:01:26 +0300

    simple cli

---
+++ cli/README.md
@@ -0,0 +1,7 @@
+# CLI
+
+This ClI is necessary to run the layer8 middleware server. It takes in a configuration file and starts the server.
+
+## How To Use
+
+
--- cli/src/main.rs
+++ cli/src/main.rs
@@ -1,3 +1,17 @@
+use clap::Parser;
+
+/// Simple cli to forward http requests to using the forward-proxy crate
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+struct Args {
+    /// Port to forward the requests to after processing
+    #[arg(short, long)]
+    port: u16,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = Args::parse();
+    if args.port.eq(&0) {
+        println!("Port cannot be 0");
+    }
 }
